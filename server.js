0720277b618dfe2a14653b4394481b28cb8ba78d
Steven Inouye <steveninouye@msn.com>
2018-12-26 20:35:59 -1000

    Start server file with root route with response "Hello World"

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+   console.log(`Server listening on port ${PORT}`)
+})
