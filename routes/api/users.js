178ccebd1298efdf77636e9eaf646516a66cc916
Steven Inouye <steveninouye@msn.com>
2018-12-27 08:20:04 -1000

    Add secretOrKey to config

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/Users');
 const bcrypt = require('bcryptjs');
+const keys = require('../../config/keys');
 
 router.get('/register', (req, res) => {
    User.findOne({ email: req.body.email }).then((user) => {
