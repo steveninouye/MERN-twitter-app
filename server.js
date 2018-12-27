3f4d3a262099d2274768ae58213a6b83b29a5e3b
Steven Inouye <steveninouye@msn.com>
2018-12-27 07:24:41 -1000

    Wrote user register route.  Registers user and hashes password

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const bp = require('body-parser');
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const User = require('./models/Users');
+
+app.use(bp.urlencoded({ extended: false }));
+app.use(bp.json());
 
 mongoose
    .connect(
@@ -12,8 +19,17 @@ mongoose
    .catch((err) => console.log(`DB Error: ${err}`));
 
 app.get('/', (req, res) => {
+   const user = new User({
+      handle: 'jim',
+      email: 'jim@jim.com',
+      password: 'jimisgreat123'
+   });
+   user.save();
    res.send('Hello a/A');
 });
+
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
